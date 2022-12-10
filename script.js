5e2af418f160ae613c13f036b69aeece84075f41
karimliaghabala <agabala.93@gmail.com>
2022-12-10 14:12:08 +0400

    updata script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const rubLeft = document.querySelector('.rubLeft');
 const usdLeft = document.querySelector('.usdLeft');
 const eurLeft = document.querySelector('.eurLeft');
-const gbrLeft = document.querySelector('.gbrLeft');
+const aznLeft = document.querySelector('.aznLeft');
 const valyutaP1 = document.querySelector('.valyuta1');
 const valyutaP2 = document.querySelector('.valyuta2');
 const firstInput = document.querySelector('.firstInput');
@@ -10,7 +10,7 @@ const secondInput = document.querySelector('.secondInput');
 const rubRight = document.querySelector('.rubRight');
 const usdRight = document.querySelector('.usdRight');
 const eurRight = document.querySelector('.eurRight');
-const gbrRight = document.querySelector('.gbrRight');
+const aznRight = document.querySelector('.aznRight');
 
 rubLeft.addEventListener('click', () => {
     fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
@@ -33,13 +33,51 @@ rubLeft.addEventListener('click', () => {
     rubLeft.style.color = "white"
     usdRight.style.background = "#833AE0"
     usdRight.style.color = "white"
+    aznRight.style.background = "white"
+    aznRight.style.color = "#8d8787"
+    eurRight.style.background = "white"
+    eurRight.style.color = "#8d8787"
 
 })
-eurRight.addEventListener('click', () => {
-    fetch('https://api.exchangerate.host/latest?base=EUR&symbols')
+
+usdRight.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
         .then(resp => resp.json())
         .then(data => {
-            valyutaP1.textContent = `1 RUB = ${data.rates.EUR} EUR`
+                        fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
+            .then(resp => resp.json())
+            .then(data => {
+                valyutaP1.textContent = `1 RUB = ${data.rates.EUR} EUR`
+            })
+            firstInput.addEventListener('keyup', () => {
+                secondInput.value = firstInput.value * data.rates.USD
+            })
+            secondInput.addEventListener('keyup', () => {
+                firstInput.value = firstInput.value * data.rates.USD
+            })
+            fetch('https://api.exchangerate.host/latest?base=USD&symbols')
+                .then(resp => resp.json())
+                .then(data => {
+                    valyutaP2.textContent = `1 USD = ${data.rates.RUB} RUB`
+                })
+        })
+    usdRight.style.background = "#833AE0"
+    eurRight.style.background = "white"
+    eurRight.style.color = "#8d8787"
+    usdRight.style.color = "white"
+    aznRight.style.background = "white"
+    aznRight.style.color = "#8d8787"
+})
+
+eurRight.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
+        .then(resp => resp.json())
+        .then(data => {
+            fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
+            .then(resp => resp.json())
+            .then(data => {
+                valyutaP1.textContent = `1 RUB = ${data.rates.EUR} EUR`
+            })
             firstInput.addEventListener('keyup', () => {
                 secondInput.value = firstInput.value * data.rates.EUR
             })
@@ -54,30 +92,36 @@ eurRight.addEventListener('click', () => {
         })
     usdRight.style.background = "white"
     eurRight.style.background = "#833AE0"
+    aznRight.style.background = "white"
+    aznRight.style.color = "#8d8787"
     eurRight.style.color = "white"
     usdRight.style.color = "#8d8787"
 })
 
 
-gbrRight.addEventListener('click', () => {
-    fetch('https://api.exchangerate.host/latest?base=GBR&symbols')
+aznRight.addEventListener('click', () => {
+    fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
         .then(resp => resp.json())
         .then(data => {
-            valyutaP1.textContent = `1 RUB = ${data.rates.GBR} GBR`
+            fetch('https://api.exchangerate.host/latest?base=RUB&symbols')
+            .then(resp => resp.json())
+            .then(data => {
+                valyutaP1.textContent = `1 RUB = ${data.rates.AZN} AZN`
+            })
             firstInput.addEventListener('keyup', () => {
-                secondInput.value = firstInput.value * data.rates.GBR
+                secondInput.value = firstInput.value * data.rates.AZN
             })
             secondInput.addEventListener('keyup', () => {
-                firstInput.value = firstInput.value * data.rates.GBR
+                firstInput.value = firstInput.value * data.rates.AZN
             })
-            fetch('https://api.exchangerate.host/latest?base=GBR&symbols')
+            fetch('https://api.exchangerate.host/latest?base=AZN&symbols')
                 .then(resp => resp.json())
                 .then(data => {
-                    valyutaP2.textContent = `1 GBR = ${data.rates.RUB} RUB`
+                    valyutaP2.textContent = `1 AZN = ${data.rates.RUB} RUB`
                 })
         })
-    gbrRight.style.background = "#833AE0"
-    gbrRight.style.color = "white"
+    aznRight.style.background = "#833AE0"
+    aznRight.style.color = "white"
     usdRight.style.background = "white"
     eurRight.style.background = "white"
     eurRight.style.color = "#8d8787"
